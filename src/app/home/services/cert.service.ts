47d44a3b31167d1d220d00b757e76fac1a399349
Srijan Mukherjee <sinjarthebeast666@gmail.com>
2017-02-09 18:21:54 +0530

    Flexbox for carousel slide container

--- src/app/home/services/cert.service.ts
+++ src/app/home/services/cert.service.ts
@@ -9,7 +9,7 @@ export class CertificationDataService   {
             title: "Hadoop Programming Level 2",
             href: "https://www.youracclaim.com/badges/b9df53f0-a223-4c19-bbfc-9410c08e8135/public_url",
             imageUrl: require("assets/img/cert/hadoop-programming-level-2.png"),
-            description: "some abcnkdjgvbdkjlvblkjb"
+            description: "This badge earner demonstrates the ability to use programming concepts provided by Apache MapReduce, Apache Pig and Apache Kafka."
         }
     ];
 
